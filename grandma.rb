18fc773d2e47ef087f0cd9d4d7a6e8973c2b88c3
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-25 04:21:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "#{phrase}.downcase"
+  elsif phrase == (#{phrase}.downcase)
    "HUH?! SPEAK UP, SONNY!"
  else phrase == "WHAT DID YOU EAT TODAY?"
    "NO, NOT SINCE 1938!"
