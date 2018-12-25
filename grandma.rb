9b7aa71af51d9a72eea50fd73f58da1eb1caaa6b
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-25 04:22:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,9 @@ def speak_to_grandma(phrase)
    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi!"
    "HUH?! SPEAK UP, SONNY!"
-   elsif phrase == "Hi Nana, how are you?"
- else phrase == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == "Hi Nana, how are you?"
+   "HUH?! SPEAK UP, SONNY!"
+  else phrase == "WHAT DID YOU EAT TODAY?"
    "NO, NOT SINCE 1938!"
 
   end
